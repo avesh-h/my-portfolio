65e14c168d27b7cce450837c2bc65e63f103df46
rishi-mumbaikar <rhmumbaikar@gmail.com>
2024-05-19 11:29:55 +0530

    FEAT: Added Project Info Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 import Hero from './pages/Hero'; 
+import DetailedProjectPageWrapper from './pages/DetailedProjectPageWrapper'
+ 
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
       <CssBaseline />
       <Routes>
         <Route path="/" element={<Hero />} />
+        <Route exact path="/project/:projectId" target="_blank" rel="noopener noreferrer" component={DetailedProjectPageWrapper} />
       </Routes>
     </Router>
   );
---
+++ src/pages/ProjectListingPage.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Container, Typography, Grid, Card, CardContent } from '@mui/material';
+import { Link } from 'react-router-dom';
+import projectsData from '../json/projectsData.json';
+
+const styles = {
+  cardContainer: {
+    backgroundColor: '#111',
+    color: '#fff',
+    marginBottom: 1,
+    border: 'none'
+  },
+  link: {
+    textDecoration: 'none', 
+    color: 'white' 
+  }
+};
+
+const ProjectListingPage = () => {
+  return (
+    <Container sx={{marginTop:"20px"}}>
+      <Typography variant="h4" color="white" gutterBottom>
+        Projects
+      </Typography>
+      <Grid container spacing={1}>
+        {projectsData.projects.map((project, index) => (
+          <Grid item xs={12} key={index}>
+            <Card variant="outlined" sx={styles.cardContainer}>
+              <CardContent>
+                <Typography variant="h5" gutterBottom>
+                  <Link to={`/project/${project.id}`} style={styles.link} target="_blank" rel="noopener noreferrer">{project.name}</Link>
+                </Typography>
+                <Typography variant="body2">{project.summary}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default ProjectListingPage;
