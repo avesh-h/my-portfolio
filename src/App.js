e5a40a303ff42a51d8aae6cab12754c90bdd29b9
Avesh <aveshhasanfatta1155@gmail.com>
2024-05-19 12:10:10 +0530

    fixed: project listing issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { CssBaseline } from '@mui/material';
-import Hero from './pages/Hero'; 
-import DetailedProjectPageWrapper from './pages/DetailedProjectPageWrapper'
- 
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import Hero from "./pages/Hero";
+import DetailedProjectPageWrapper from "./pages/DetailedProjectPageWrapper";
 
 const App = () => {
   return (
@@ -11,7 +10,10 @@ const App = () => {
       <CssBaseline />
       <Routes>
         <Route path="/" element={<Hero />} />
-        <Route exact path="/project/:projectId" target="_blank" rel="noopener noreferrer" component={DetailedProjectPageWrapper} />
+        <Route
+          path={"/project/:projectId"}
+          element={<DetailedProjectPageWrapper />}
+        />
       </Routes>
     </Router>
   );
